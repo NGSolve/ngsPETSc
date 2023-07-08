edd7a18106e218879a6fb2b44aa70b8633d3cc84
Stefano Zampini <stefano.zampini@gmail.com>
2023-07-09 01:18:24 +0200

    Matrix: handle non-square matrices
    use AIJ if blocksize is 1
    cleanup code
    add test for nonsquare matrices

--- tests/test_mat.py
+++ tests/test_mat.py
@@ -1,13 +1,13 @@
 '''
 This module test the matrix class
 '''
-from ngsolve import Mesh, H1, BilinearForm, grad, dx
+from ngsolve import Mesh, H1, VectorH1, BilinearForm, grad, div, dx, VOL
 from netgen.geom2d import unit_square
 import netgen.meshing as ngm
 
 from mpi4py.MPI import COMM_WORLD
-
 from ngsPETSc import Matrix
+from petsc4py import PETSc
 
 def test_poisson_mat():
     '''
@@ -21,3 +21,27 @@ def test_poisson_mat():
     u,v = fes.TnT()
     a = BilinearForm(grad(u)*grad(v)*dx).Assemble()
     Matrix(a.mat, fes.FreeDofs())
+
+def test_nonsquare_mat():
+    '''
+    Testing that a nonsquare matrix is correctly exported.
+    '''
+    if COMM_WORLD.rank == 0:
+        mesh = Mesh(unit_square.GenerateMesh(maxh=0.1).Distribute(COMM_WORLD))
+    else:
+        mesh = Mesh(ngm.Mesh.Receive(COMM_WORLD))
+
+    V = VectorH1(mesh, order=2, dirichlet="left|right|top|bottom")
+    Q = H1(mesh, order=1)
+
+    u,_ = V.TnT()
+    _,q = Q.TnT()
+
+    b = BilinearForm(trialspace=V, testspace=Q)
+    b += div(u)*q*dx
+    b.Assemble()
+    Matrix(b.mat, (Q.FreeDofs(), V.FreeDofs()))
+
+if __name__ == '__main__':
+    test_poisson_mat()
+    test_nonsquare_mat()
