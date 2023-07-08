8e6703d6bd4c98c81d1dd268dba7829ca1739214
Umberto Zerbinati <umberto.zerbinati@maths.ox.ac.uk>
2023-07-08 12:19:37 -0700

    Added pc class and fixed what is need for it in vec class
    Signed-off-by: Umberto Zerbinati <umberto.zerbinati@maths.ox.ac.uk>

--- ngsPETSc/__init__.py
+++ ngsPETSc/__init__.py
@@ -5,8 +5,9 @@ from ngsPETSc.mat import *
 from ngsPETSc.vec import *
 from ngsPETSc.plex import *
 from ngsPETSc.ksp import *
+from ngsPETSc.pc import *
 from ngsPETSc.eps import *
 
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 
 __all__ = ["Matrix","VectorMapping","MeshMapping","KrylovSolver","EigenSolver"]
