627f09a09b5ef017f74f11c4a95d771fb597992a
Stefano Zampini <stefano.zampini@gmail.com>
2023-07-09 01:18:24 +0200

    Matrix: handle non-square matrices
    use AIJ if blocksize is 1
    cleanup code
    add test for nonsquare matrices

--- ngsPETSc/mat.py
+++ ngsPETSc/mat.py
@@ -11,66 +11,94 @@ from ngsolve import MPI_Init
 
 class Matrix(object):
     '''
-    This calss creates a sparse PETSc Matrix
+    This class creates a PETSc Matrix
 
     :arg ngsMat: the NGSolve matrix
 
-    :arg freeDofs: free DOFs of the FE space used to construct the matrix
+    :arg freeDofs: free DOFs of the FE spaces used to construct the matrix
 
-    :arg matType: type of sparse matrix, i.e. PETSc sparse: aij, 
-    MKL sparse: mklaij or CUDA: aijcusparse
+    :arg matType: type of PETSc matrix, i.e. PETSc sparse: aij,
+    MKL sparse: aijmkl or CUDA: aijcusparse
 
     '''
     def __init__(self, ngsMat, freeDofs=None, matType="aij"):
-        self.freeDofs = freeDofs
+        if not isinstance(freeDofs, (tuple, list)):
+            samerc = True
+            self.freeDofs = (freeDofs, freeDofs)
+        else:
+            samerc = False
+            self.freeDofs = freeDofs
         if hasattr(ngsMat, 'row_paradofs'):
             self.comm = ngsMat.paradofs.comm.mpi4py
         else:
             self.comm = MPI_Init().mpi4py
+
         localMat = ngsMat.local_mat
         entryHeight, entryWidth = localMat.entrysizes
         if entryHeight != entryWidth: raise RuntimeError ("Only square entries are allowed.")
-        valMat,colMat,indMat = localMat.CSR()
+
+        valMat, colMat, indMat = localMat.CSR()
         indMat = np.array(indMat).astype(PETSc.IntType)
         colMat = np.array(colMat).astype(PETSc.IntType)
-        petscLocalMat = PETSc.Mat().createBAIJ(size=(entryHeight*localMat.height,
-                                                     entryWidth*localMat.width),
-                                               bsize=entryHeight,
-                                               csr=(indMat,colMat,valMat),
-                                               comm=MPI.COMM_SELF)
-        if self.freeDofs is not None:
-            localMatFree = np.flatnonzero(self.freeDofs).astype(PETSc.IntType)
-            isFreeLocal = PETSc.IS().createBlock(indices=localMatFree, bsize=entryHeight)
-            petscLocalMat = petscLocalMat.createSubMatrices(isFreeLocal)[0]
+        if entryHeight > 1:
+            petscLocalMat = PETSc.Mat().createBAIJ(size=(entryHeight*localMat.height,
+                                                         entryWidth*localMat.width),
+                                                   bsize=entryHeight,
+                                                   csr=(indMat,colMat,valMat),
+                                                   comm=PETSc.COMM_SELF)
+        else:
+            petscLocalMat = PETSc.Mat().createAIJ(size=(localMat.height,
+                                                        localMat.width),
+                                                  csr=(indMat,colMat,valMat),
+                                                  comm=PETSc.COMM_SELF)
+        if self.freeDofs[0] is not None or self.freeDofs[1] is not None:
+            rowIsFreeLocal = None
+            if self.freeDofs[0] is not None:
+                rowLocalMatFree = np.flatnonzero(self.freeDofs[0]).astype(PETSc.IntType)
+                rowIsFreeLocal = PETSc.IS().createBlock(indices=rowLocalMatFree, bsize=entryHeight,comm=PETSc.COMM_SELF)
+            colIsFreeLocal = rowIsFreeLocal
+            if self.freeDofs[1] is not None and not samerc:
+                colLocalMatFree = np.flatnonzero(self.freeDofs[1]).astype(PETSc.IntType)
+                colIsFreeLocal = PETSc.IS().createBlock(indices=colLocalMatFree, bsize=entryHeight,comm=PETSc.COMM_SELF)
+            petscLocalMat = petscLocalMat.createSubMatrices(rowIsFreeLocal, colIsFreeLocal)[0]
 
         if self.comm.Get_size() > 1:
-            matTypes = {"aij":"mpiaij", "aijcusparse":"mpiaijcusparse",
-                        "aijmkl":"mpiaijmkl", "dense":"dense"}
-            parallelDofs = ngsMat.row_pardofs
-            globalNums, numberGlobal = parallelDofs.EnumerateGlobally(self.freeDofs)
-            if self.freeDofs is not None:
-                globalNums = np.array(globalNums, dtype=PETSc.IntType)[self.freeDofs]
-                localGlobalMap = PETSc.LGMap().create(indices=globalNums,
-                                                      bsize=entryHeight,
-                                                      comm=self.comm)
-                mat = PETSc.Mat().create(comm=self.comm)
-                mat.setSizes(size=numberGlobal*entryHeight, bsize=entryHeight)
-                mat.setType(PETSc.Mat.Type.IS)
-                mat.setLGMap(localGlobalMap)
-                mat.setISLocalMat(petscLocalMat)
-                mat.assemble()
-                self.type = matTypes[matType]
-                mat.convert(self.type)
-                self.mat = mat
+            # is this a BUG in the bindings?
+            #rparallelDofs = ngsMat.row_pardofs
+            rparallelDofs = ngsMat.col_pardofs
+            rglobalNums, rnumberGlobal = rparallelDofs.EnumerateGlobally(self.freeDofs[0])
+            if self.freeDofs[0] is not None:
+                rglobalNums = np.array(rglobalNums, dtype=PETSc.IntType)[self.freeDofs[0]]
+            rlocalGlobalMap = PETSc.LGMap().create(indices=rglobalNums,
+                                                   bsize=entryHeight,
+                                                   comm=self.comm)
+            if not samerc:
+                # is this a BUG in the bindings?
+                #cparallelDofs = ngsMat.col_pardofs
+                cparallelDofs = ngsMat.row_pardofs
+                cglobalNums, cnumberGlobal = cparallelDofs.EnumerateGlobally(self.freeDofs[1])
+                if self.freeDofs[1] is not None:
+                    cglobalNums = np.array(cglobalNums, dtype=PETSc.IntType)[self.freeDofs[1]]
+                clocalGlobalMap = PETSc.LGMap().create(indices=cglobalNums,
+                                                       bsize=entryWidth,
+                                                       comm=self.comm)
+            else:
+                clocalGlobalMap = rlocalGlobalMap
+                cnumberGlobal = rnumberGlobal
+
+            mat = PETSc.Mat().create(comm=self.comm)
+            mat.setSizes(size=(rnumberGlobal*entryHeight, cnumberGlobal*entryHeight), bsize=entryHeight)
+            mat.setType(PETSc.Mat.Type.IS)
+            mat.setLGMap(rlocalGlobalMap, clocalGlobalMap)
+            mat.setISLocalMat(petscLocalMat)
+            mat.assemble()
+            mat.convert(matType)
+            self.mat = mat
         else:
-            if self.freeDofs is not None:
-                mat = petscLocalMat
-                mat.assemble()
-                matTypes = {"aij":"seqaij", "aijcusparse":"seqaijcusparse",
-                            "aijmkl":"seqaijmkl", "dense":"dense"}
-                self.type = matTypes[matType]
-                mat.convert(self.type)
-                self.mat = mat
+            mat = petscLocalMat
+            mat.convert(matType)
+            self.mat = mat
+
     def view(self):
         '''
         This function display PETSc Mat info
