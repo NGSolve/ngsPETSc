3761cab9bc0195845c93d6d1fdf400726a82c721
Umberto Zerbinati <zerbinati@maths.ox.ac.uk>
2024-02-08 23:26:24 +0000

    Fix python version
    Signed-off-by: Umberto Zerbinati <zerbinati@maths.ox.ac.uk>

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     packages=["ngsPETSc", "ngsPETSc.utils"],
-    python_requires = ">=3.10",
+    python_requires = ">=3.9",
     install_requires=install_requires
 
 )
