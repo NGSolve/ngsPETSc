a0d5fa70e146706a1cf9b155497d34390bfe7c2b
Umberto Zerbinati <zerbinati@maths.ox.ac.uk>
2024-05-28 14:51:22 +0100

    Import firedrake from the side
    Signed-off-by: Umberto Zerbinati <zerbinati@maths.ox.ac.uk>

--- ngsPETSc/utils/firedrake/pml.py
+++ ngsPETSc/utils/firedrake/pml.py
@@ -3,21 +3,18 @@ This file contains all the functions related to creating a
 Perfectly Matched Layer (PML) in Firedrake.
 '''
 from collections.abc import Iterable
-from firedrake import Constant, Function, solve, DirichletBC, TrialFunction, TestFunction
-from firedrake import inner, grad, dx, conditional, real, FunctionSpace
-from firedrake import ufl
-from firedrake.__future__ import interpolate
+import firedrake as fd
 
-class WeightedMeasure(ufl.Measure):
+class WeightedMeasure(fd.ufl.Measure):
     """
     This class creates a weighted measure in Firedrake.
     """
     def __init__(self, *args, weight=None, **kwargs):
         self.weight = weight
-        ufl.Measure.__init__(self, *args, **kwargs)
+        fd.ufl.Measure.__init__(self, *args, **kwargs)
 
     def __rmul__(self, integrand):
-        return ufl.Measure.__rmul__(self, self.weight * integrand)
+        return fd.ufl.Measure.__rmul__(self, self.weight * integrand)
 
 class PML:
     """
@@ -35,7 +32,7 @@ class PML:
                 coefficient for one of the PML regions.
     :arg solver_parameters: The solver parameters for the PML problem.
     """
-    def __init__(self, mesh, order, pml_regions, k, alpha=Constant(1j), solver_parameters=None):
+    def __init__(self, mesh, order, pml_regions, k, alpha=fd.Constant(1j), solver_parameters=None):
         """
         This function initializes the PML object.
         """
@@ -53,20 +50,20 @@ class PML:
                         range(len(mesh.netgen_mesh.GetRegionNames(dim=1)))))
         labels2 = dict(map(lambda i,j : (i,j+1) , mesh.netgen_mesh.GetRegionNames(dim=2),
                         range(len(mesh.netgen_mesh.GetRegionNames(dim=2)))))
-        if not isinstance(k, (Constant, Function, list)):
+        if not isinstance(k, (fd.Constant, fd.Function, list)):
             raise ValueError("The wavenumber must be a Constant, a Function, or a list.")
         if not isinstance(k, Iterable):
             k = [k]*len(pml_regions)
-        if not isinstance(alpha, (Constant, Function, list)):
+        if not isinstance(alpha, (fd.Constant, fd.Function, list)):
             raise ValueError("The attenuation coefficient must be a \
                              Constant, a Function, or a list.")
         if not isinstance(alpha, Iterable):
             alpha = [alpha]*len(pml_regions)
         #Construct the PML for each PML region
         self.pmls = []
-        self.V = FunctionSpace(self.mesh, "CG", self.order)
-        u = TrialFunction(self.V)
-        v = TestFunction(self.V)
+        self.V = fd.FunctionSpace(self.mesh, "CG", self.order)
+        u = fd.TrialFunction(self.V)
+        v = fd.TestFunction(self.V)
         for region in self.pml_regions:
             if region[0] not in labels2:
                 raise ValueError("The PML region name must be a valid region name.")
@@ -75,19 +72,19 @@ class PML:
             if region[2] not in labels1:
                 raise ValueError("The PML outer boundary name must be a valid region name.")
             #Construct the weight function for the PML
-            F = inner(grad(u), grad(v))*dx(labels2[region[0]])
+            F = fd.inner(fd.grad(u), fd.grad(v))*fd.dx(labels2[region[0]])
             for i in range(1, len(self.mesh.netgen_mesh.GetRegionNames(dim=2))+1):
                 if i != labels2[region[0]]:
-                    F += inner(grad(u), grad(v))*dx(i)
-            L = inner(Constant(1),v)*dx(2)
-            bcs = [DirichletBC(self.V, 1, labels1[region[1]]),
-                   DirichletBC(self.V, 0, labels1[region[2]])]
-            dalet = Function(self.V)
-            solve(F == L, dalet, bcs=bcs, solver_parameters=solver_parameters)
-            sigma = interpolate(1+(alpha/k)*dalet, self.V)
-            self.pmls = self.pmls + [conditional(real(sigma) < 1e-12, 1, sigma)]
+                    F += fd.inner(fd.grad(u), fd.grad(v))*fd.dx(i)
+            L = fd.inner(fd.Constant(1),v)*fd.dx(labels2[region[0]])
+            bcs = [fd.DirichletBC(self.V, 1, labels1[region[1]]),
+                   fd.DirichletBC(self.V, 0, labels1[region[2]])]
+            dalet = fd.Function(self.V)
+            fd.solve(F == L, dalet, bcs=bcs, solver_parameters=solver_parameters)
+            sigma = fd.__future__.interpolate(1+(alpha/k)*dalet, self.V)
+            self.pmls = self.pmls + [fd.conditional(fd.real(sigma) < 1e-12, 1, sigma)]
             #Assembling the weighted measure
-            weight = Function(self.V)
+            weight = fd.Function(self.V)
             for pml in self.pmls:
                 weight = weight+pml
             self.dx = WeightedMeasure("dx", weight=weight)
