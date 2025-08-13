388f4c838e7e82120f9cace8c902ff1f2cd83d6f
Jørgen S. Dokken <dokken@simula.no>
2025-08-13 10:08:41 +0000

    Update tests

--- ngsPETSc/utils/fenicsx.py
+++ ngsPETSc/utils/fenicsx.py
@@ -6,13 +6,14 @@ the package will only be used in combination with FEniCSx.
 import typing
 import dolfinx
 import numpy as np
-
+from packaging.version import Version
 from mpi4py import MPI as _MPI
 
 from ngsPETSc import MeshMapping
 
 # Map from Netgen cell type (integer tuple) to GMSH cell type
 _ngs_to_cells = {(2,3): 2, (2,4):3, (3,4): 4}
+
 
 class GeometricModel:
     """
@@ -21,16 +22,18 @@ class GeometricModel:
             geo: The Netgen model
             comm: The MPI communicator to use for mesh creation
     """
-    def __init__(self,geo, comm: _MPI.Comm):
+    def __init__(self,geo, comm: _MPI.Comm, comm_rank:int = 0):
         self.geo = geo
         self.comm = comm
+        self.comm_rank = comm_rank
 
     def model_to_mesh(self, hmax: float, gdim: int = 2,
         partitioner: typing.Callable[
         [_MPI.Comm, int, int, dolfinx.cpp.graph.AdjacencyList_int32],
         dolfinx.cpp.graph.AdjacencyList_int32] =
         dolfinx.mesh.create_cell_partitioner(dolfinx.mesh.GhostMode.none),
-        transform: typing.Any = None, routine: typing.Any = None) -> dolfinx.mesh.Mesh:
+        transform: typing.Any = None, routine: typing.Any = None
+        ) -> tuple[dolfinx.mesh.Mesh, dolfinx.mesh.MeshTags, dict[str, list[int]]]:
         """Given a NetGen model, take all physical entities of the highest
         topological dimension and create the corresponding DOLFINx mesh.
 
@@ -50,12 +53,14 @@ class GeometricModel:
         Returns:
             A DOLFINx mesh for the given NetGen model.
         """
+        # To be parallel safe, we generate on all processes
+        # NOTE: This might change in the future.
+
         # First we generate a mesh
         ngmesh = self.geo.GenerateMesh(maxh=hmax)
         # Apply any ngs routine post meshing
         if routine is not None:
             ngmesh, self.geo = routine(ngmesh, self.geo)
-        # Applying any PETSc Transform
         if transform is not None:
             meshMap = MeshMapping(ngmesh)
             transform.setDM(meshMap.plex)
@@ -63,16 +68,36 @@ class GeometricModel:
             newplex = transform.apply(meshMap.plex)
             meshMap = MeshMapping(newplex)
             ngmesh = meshMap.ngmesh
-        # We extract topology and geometry
+
+        assert ngmesh.dim in (2, 3), "Only 2D and 3D meshes are supported."
+        _dim_to_element_wrapper = {
+            1: ngmesh.Elements1D,
+            2: ngmesh.Elements2D,
+            3: ngmesh.Elements3D}
+
         V, T = None, None
-        if ngmesh.dim == 2:
+        if self.comm.rank == self.comm_rank:
+
+            # Applying any PETSc Transform
+            # We extract topology and geometry
             V = ngmesh.Coordinates()
-            T = ngmesh.Elements2D().NumPy()["nodes"]
-            T = np.array([list(np.trim_zeros(a, 'b')) for a in list(T)])-1
-        elif ngmesh.dim == 3:
-            V = ngmesh.Coordinates()
-            T = ngmesh.Elements3D().NumPy()["nodes"]
-            T = np.array([list(np.trim_zeros(a, 'b')) for a in list(T)])-1
+            T = _dim_to_element_wrapper[ngmesh.dim]().NumPy()["nodes"]
+            if Version(np.__version__) >= Version("2.2"):
+                T = np.trim_zeros(T, "b", axis=1).astype(np.int64) - 1
+            else:
+                T = (
+                    np.array(
+                        [list(np.trim_zeros(a, "b")) for a in list(T)],
+                        dtype=np.int64,
+                    )
+                    - 1
+                )
+        else:
+            # NOTE: For mixed meshes, this must change
+            V = np.zeros((0, ngmesh.dim), dtype=np.float64)
+            T = np.zeros((0, ngmesh.dim+1), dtype=np.int64)
+
+        # NOTE: Here we should curve meshes
         ufl_domain = dolfinx.io.gmshio.ufl_mesh(
             _ngs_to_cells[(gdim,T.shape[1])], gdim, dolfinx.default_real_type)
         cell_perm = dolfinx.cpp.io.perm_gmsh(dolfinx.cpp.mesh.to_type(str(ufl_domain.ufl_cell())),
@@ -80,4 +105,45 @@ class GeometricModel:
         T = np.ascontiguousarray(T[:, cell_perm])
         mesh = dolfinx.mesh.create_mesh(self.comm, cells=T, x=V, e=ufl_domain,
                                         partitioner=partitioner)
-        return mesh
+
+        if self.comm.rank == self.comm_rank:
+            regions: dict[str, list[int]] = {name: []
+                                             for name in ngmesh.GetRegionNames(dim=ngmesh.dim-1)}
+            for i, name in enumerate(ngmesh.GetRegionNames(ngmesh.dim-1), 1):
+                regions[name].append(i)
+
+            ng_facets = _dim_to_element_wrapper[ngmesh.dim-1]()
+            facet_indices = ng_facets.NumPy()["nodes"].astype(np.int64)
+            if Version(np.__version__) >= Version("2.2"):
+                facets = np.trim_zeros(facet_indices, "b", axis=1).astype(np.int64) - 1
+            else:
+                facets = (
+                    np.array(
+                        [list(np.trim_zeros(a, "b")) for a in list(facet_indices)],
+                        dtype=np.int64,
+                    )
+                    - 1
+                )
+            # Can't use the vectorized version, due to a bug in ngsolve:
+            # https://forum.ngsolve.org/t/extract-facet-markers-from-netgen-mesh/3256
+            facet_values = np.array([facet.index for facet in ng_facets], dtype=np.int32)
+            regions = self.comm.bcast(regions, root=0)
+        else:
+            # NOTE: Mixed meshes on non-simplex geometries requires changes
+            facets = np.zeros((0, ngmesh.dim), dtype=np.int64)
+            facet_values = np.zeros((0,), dtype=np.int32)
+            regions = self.comm.bcast(None, root=0)
+
+        local_entities, local_values = dolfinx.io.gmshio.distribute_entity_data(
+            mesh, mesh.topology.dim - 1, facets, facet_values
+        )
+        mesh.topology.create_connectivity(mesh.topology.dim - 1, 0)
+        adj = dolfinx.graph.adjacencylist(local_entities)
+        ft = dolfinx.mesh.meshtags_from_entities(
+            mesh,
+            mesh.topology.dim - 1,
+            adj,
+            local_values.astype(np.int32, copy=False),
+        )
+        ft.name = "Facet tags"
+        return mesh, ft, regions
--- tests/test_fenicsx.py
+++ tests/test_fenicsx.py
@@ -3,6 +3,7 @@ This module test the utils.fenicsx class
 '''
 import pytest
 
+
 def test_square_netgen():
     '''
     Testing FEniCSx interface with Netgen generating a square mesh
@@ -18,7 +19,7 @@ def test_square_netgen():
     geo = SplineGeometry()
     geo.AddRectangle((0,0),(1,1))
     geoModel = ngfx.GeometricModel(geo, MPI.COMM_WORLD)
-    domain  =geoModel.model_to_mesh(hmax=0.1)
+    domain, _, _  = geoModel.model_to_mesh(hmax=0.1)
     with XDMFFile(domain.comm, "XDMF/mesh.xdmf", "w") as xdmf:
         xdmf.write_mesh(domain)
 
@@ -42,7 +43,7 @@ def test_poisson_netgen():
     geo = SplineGeometry()
     geo.AddRectangle((0,0),(np.pi,np.pi))
     geoModel = ngfx.GeometricModel(geo, MPI.COMM_WORLD)
-    msh  = geoModel.model_to_mesh(hmax=0.1)
+    msh, _, _  = geoModel.model_to_mesh(hmax=0.1)
     V = fem.functionspace(msh, ("Lagrange", 2)) #pylint: disable=E1120
     facetsLR = mesh.locate_entities_boundary(msh, dim=(msh.topology.dim - 1),
              marker=lambda x: np.logical_or(np.isclose(x[0], 0.0),
@@ -69,6 +70,42 @@ def test_poisson_netgen():
                   petsc_options_prefix="test_solver")
     problem.solve()
 
+
+def test_crack_netgen():
+    try:
+        from mpi4py import MPI
+        import ngsPETSc.utils.fenicsx as ngfx
+        from netgen.geom2d import CSG2d, Solid2d, EdgeInfo, Circle
+    except ImportError:
+        pytest.skip("DOLFINx unavailable, skipping FENICSx test")
+    geo = CSG2d()
+    poly = Solid2d(
+        [
+            (0, 0),
+            EdgeInfo(bc="bottom"),
+            (2, 0),
+            EdgeInfo(bc="right"),
+            (2, 2),
+            EdgeInfo(bc="topright"),
+            (1.01, 2),
+            EdgeInfo(bc="crackright"),
+            (1, 1.5),
+            EdgeInfo(bc="crackleft"),
+            (0.99, 2),
+            EdgeInfo(bc="topleft"),
+            (0, 2),
+            EdgeInfo(bc="left"),
+        ]
+    )
+
+    disk = Circle((0.3, 0.3), 0.2, bc="hole")
+    geo.Add(poly - disk)
+
+    geoModel = ngfx.GeometricModel(geo, MPI.COMM_WORLD)
+    domain, ft, region_map = geoModel.model_to_mesh(hmax=0.1)
+
+
 if __name__ == "__main__":
     test_square_netgen()
     test_poisson_netgen()
+    test_crack_netgen()
