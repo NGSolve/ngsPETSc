e386bfd48014acc2e41eb7202a12196a871e6a80
Stefano Zampini <stefano.zampini@gmail.com>
2025-10-13 22:51:09 +0300

    use command line option to initialize petsc4py

--- ngsPETSc/__init__.py
+++ ngsPETSc/__init__.py
@@ -1,7 +1,13 @@
 '''
 ngsPETSc is a NGSolve/Netgen interface to PETSc
 '''
-from ngsPETSc.plex import *
+#initialize PETSc first
+import sys
+import petsc4py
+
+petsc4py.init(sys.argv)
+
+from ngsPETSc.plex import * #pylint: disable=C0413
 
 __all__ = []
 
