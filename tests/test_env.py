ed5320f18be7aa15c22adf266257f2bd771c7778
Pablo Brubeck <brubeck@protonmail.com>
2026-04-09 20:46:28 +0100

    import inside the tests

--- tests/test_env.py
+++ tests/test_env.py
@@ -3,29 +3,37 @@ This module test that the environment has been setup correctly.
 In particular it will test for: petsc4py, PETSc, NGSolve and Netgen
 '''
 
-import petsc4py
-from petsc4py import PETSc
-
-import ngsolve as ngs
-from netgen.geom2d import unit_square
-from ngsolve import x,y
-
 def test_petsc4py():
     '''
     Testing that petsc4py can be imported correctly
     '''
+    import petsc4py
     assert petsc4py.get_config() != ""
+
 
 def  test_petsc():
     '''
     Testing that PETSc can be imported correctly
     '''
+    from petsc4py import PETSc
     assert PETSc.DECIDE == -1
+
+
+def test_netgen():
+    '''
+    Testing that NGSolve can be imported correctly
+    '''
+    from netgen.geom2d import unit_square
+    unit_square.GenerateMesh(maxh=0.1)
+
 
 def test_ngs():
     '''
     Testing that NGSolve can be imported correctly
     '''
+    from netgen.geom2d import unit_square
+    import ngsolve as ngs
+    from ngsolve import x,y
     mesh = ngs.Mesh(unit_square.GenerateMesh(maxh=0.1))
     coefficientFunction = x*(1-y)
     mip = mesh(0.2, 0.2)
